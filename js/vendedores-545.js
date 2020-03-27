fd3565f1985773b55a181c6f5dab709fcbfa2490
Leandro <oleito@gmail.com>
2020-03-27 17:45:46 -0300

    Agrega vue, crea vendedores

---
+++ js/vendedores-545.js
@@ -0,0 +1,28 @@
+var appVendedores = new Vue({
+  el: "#appVendedores",
+  data: {
+    vendedores: [
+      {
+        img: "Agonzales.jpg",
+        nombre: "Alejandra Gonzales",
+        sector: "Ejecutivo de ventas",
+        cel: "2664619109",
+        email: "alejandra.gonzales@parisautos.com.ar"
+      },
+      {
+        img: "Saguello.jpg",
+        nombre: "Silvana Arguello",
+        sector: "Ejecutivo de ventas",
+        cel: "2664364639",
+        email: "silvana.aguello@parisautos.com.ar"
+      },
+      {
+        img: "Pfernandez.jpg",
+        nombre: "Pamela Fernandez",
+        sector: "Recepcion",
+        cel: "2664316521",
+        email: "recepcion@parisautos.com.ar"
+      }
+    ]
+  }
+});
