f5ac651fa85d2926ab2a0ee6f6782241fabfc515
leandro mint <oleito>
2020-12-09 11:07:44 -0300

    Actualizaciones diciembre
    Agrega seccion planes

---
+++ js/modelos-planes-2563.js
@@ -0,0 +1,49 @@
+var app = new Vue({
+  el: "#appModelos",
+  data: {
+    message: "Hello Vue!",
+    modelos: [
+      {
+        img: "c4_lounge_20.jpg",
+        nombre: "C4 Lounge",
+        ficha: "FICHA_TECNICA_C4_LOUNGE_20.pdf"
+      }, 
+      {
+        img: "c4_cactus.jpg",
+        nombre: "C4 Cactus",
+        ficha: "FICHA_TECNICA_C4_CACTUS.pdf"
+      }, 
+      {
+        img: "c3.jpg",
+        nombre: "C3",
+        ficha: "FICHA_TECNICA_C3.pdf"
+      }, 
+      {
+        img: "c3_aircross.jpg",
+        nombre: "C3 Aircross",
+        ficha: "FICHA_TECNICA_C3_AIRCROSS.pdf"
+      }, 
+      {
+        img: "berlingo_m.jpg",
+        nombre: "Berlingo Multiespacio",
+        ficha: "FICHA_TECNICA_BERLINGO_M.pdf"
+      }, 
+      {
+        img: "berlingo_f.jpg",
+        nombre: "Berlingo Furgon",
+        ficha: "FICHA_TECNICA_BERLINGO_F.pdf"
+      }, 
+      {
+        img: "jumpy.jpg",
+        nombre: "Jumpy",
+        ficha: "FICHA_TECNICA_JUMPY_F.pdf"
+      }, 
+      {
+        img: "jumper.jpg",
+        nombre: "Jumper",
+        ficha: "FICHA_TECNICA_JUMPER_F.pdf"
+      },
+      
+    ]
+  }
+});
---
+++ js/vendedores-planes-070817.js
@@ -0,0 +1,35 @@
+var appVendedores = new Vue({
+  el: "#appVendedores",
+  data: {
+    vendedores: [
+      {
+        img: "Dmoreno.jpg",
+        nombre: "Diego Moreno",
+        sector: "Responsable comercial",
+        cel: "2664503609",
+        email: "diego.moreno@parisautos.com.ar",
+      },
+      {
+        img: "Jlujan.jpg",
+        nombre: "Jorge Lujan",
+        sector: "Ejecutivo de ventas",
+        cel: "2664272875",
+        email: "jorge.lujan@parisautos.com.ar",
+      },
+      {
+        img: "Mzabala.jpg",
+        nombre: "Mariana Zabala",
+        sector: "Ejecutivo de ventas",
+        cel: "2664188646",
+        email: "mariana.zabala@parisautos.com.ar",
+      },
+      {
+        img: "Adessi.jpg",
+        nombre: "Angel Dessi",
+        sector: "Ejecutivo de ventas",
+        cel: "2664304402",
+        email: "angel.dessi@parisautos.com.ar",
+      }
+    ],
+  },
+});
